b91fdbeb0aa4be20e84af199050d05caf4030863
redjstone <amadorcb09@gmail.com>
2023-10-11 22:35:09 +0200

    Spectators can NOT enter locked areas anymore.

--- server/client_manager.py
+++ server/client_manager.py
@@ -944,7 +944,6 @@ class ClientManager:
             allowed = (
                 self.is_mod
                 or self in area.owners
-                or self.char_id == -1
                 or area == area.area_manager.default_area()
             )
             if not allowed:
